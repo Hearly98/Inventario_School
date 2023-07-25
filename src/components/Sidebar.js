70fd73ab2dadedf41c8591f93c7e5a5609008383
ofic <shuertas981@gmail.com>
2023-07-25 15:19:11 -0500

    Agregando pages Maintance y sub menus desplegables

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,7 +1,24 @@
-import React from "react";
+import React, {useState} from "react";
 import { NavLink } from "react-router-dom";
 import * as FaIcons from 'react-icons/fa';
 const Sidebar = () =>{
+    const [showMantenimiento, setShowMantenimiento] = useState(false);
+    const [showLogistica, setShowLogistica] = useState(false);
+    const [showReportes, setShowReportes] = useState(false);
+  
+    // Funciones para mostrar/ocultar los submenús
+    const toggleMantenimiento = () => {
+      setShowMantenimiento(!showMantenimiento);
+    };
+  
+    const toggleLogistica = () => {
+      setShowLogistica(!showLogistica);
+    };
+  
+    const toggleReportes = () => {
+      setShowReportes(!showReportes);
+    };
+  
     return (
         <div className="w-64 h-screen bg-slate-900 text-base text-white" >
            <ul>
@@ -12,10 +29,39 @@ const Sidebar = () =>{
                 Santo tomas de Aquino
                 </li>
                 <li className="p-2 hover:bg-slate-600">
-                <NavLink to="/" exact={true} className="flex m-2" activeclassname="active"><FaIcons.FaHome className="mx-3"/>Inicio</NavLink> 
+                    <NavLink to="/" exact={true} className="flex m-2" activeclassname="active"><FaIcons.FaHome className="mx-3"/>Inicio</NavLink> 
                 </li>
-                <li className="p-2 hover:bg-slate-600">
-                <NavLink to="/logistica" exact={true} className="flex m-2" activeclassname="active"><FaIcons.FaTruck className="mx-3"/>Logistica</NavLink>
+                <li className="p-2 hover:bg-slate-600 " onClick={toggleMantenimiento}>
+                    <p className="flex m-2 cursor-pointer"><FaIcons.FaTruck className="mx-3 "/>Mantenimiento</p>
+                {showMantenimiento && (
+                <ul>
+                        <li className="p-3 hover:bg-slate-500">
+                        <NavLink to="/clientes" exact={true} className="flex" activeclassname="active"><FaIcons.FaStopCircle className="mx-3 mt-1"/>Clientes</NavLink>
+                        </li>
+                        <li className="p-3 hover:bg-slate-500">
+                        <NavLink to="/productos" exact={true} className="flex" activeclassname="active"><FaIcons.FaStopCircle className="mx-3 mt-1"/>Productos</NavLink>
+                        </li>
+                        <li className="p-3 hover:bg-slate-500">
+                        <NavLink to="/proveedores" exact={true} className="flex" activeclassname="active"><FaIcons.FaStopCircle className="mx-3 mt-1"/>Proveedores</NavLink>
+                        </li>
+                </ul>
+                )}
+                </li>
+                <li className="p-2 hover:bg-slate-600" onClick={toggleLogistica}>
+                    <p className="flex m-2" ><FaIcons.FaTruck className="mx-3"/>Logistica</p>
+                    {showLogistica && (
+                <ul>
+                        <li className="p-3 hover:bg-slate-500">
+                        <NavLink to="/ordenDeCompra" exact={true} className="flex" activeclassname="active"><FaIcons.FaStopCircle className="mx-3 mt-1"/>Orden de Compra</NavLink>
+                        </li>
+                        <li className="p-3 hover:bg-slate-500">
+                        <NavLink to="/stockAlmacen" exact={true} className="flex" activeclassname="active"><FaIcons.FaStopCircle className="mx-3 mt-1"/>Stock Almacen</NavLink>
+                        </li>
+                        <li className="p-3 hover:bg-slate-500">
+                        <NavLink to="/movimientos" exact={true} className="flex" activeclassname="active"><FaIcons.FaStopCircle className="mx-3 mt-1"/>Movimientos</NavLink>
+                        </li>
+                </ul>
+                    )}
                 </li>
                 <li className="p-2 hover:bg-slate-600">
                 <NavLink to="/tesoreria" exact={true} className="flex m-2" activeclassname="active"><FaIcons.FaCashRegister className="mx-3"/>Tesoreria</NavLink>
@@ -23,9 +69,21 @@ const Sidebar = () =>{
                 <li className="p-2 hover:bg-slate-600">
                 <NavLink to="/ventas" exact={true} className="flex m-2" activeclassname="active"><FaIcons.FaMoneyBill className="mx-3"/>Ventas</NavLink>
                 </li>
-                <li className="p-2 hover:bg-slate-600">
-                <NavLink to="/reportes" exact={true} className="flex m-2" activeclassname="active"><FaIcons.FaChartBar className="mx-3"/>Reportes</NavLink>
-
+                <li className="p-2 hover:bg-slate-600" onClick={toggleReportes}>
+                    <p className="flex m-2 cursor-pointer"><FaIcons.FaChartBar className="mx-3 "/>Reportes</p>
+                {showReportes && (
+                <ul>
+                        <li className="p-3 hover:bg-slate-500">
+                        <NavLink to="/clientes" exact={true} className="flex" activeclassname="active"><FaIcons.FaStopCircle className="mx-3 mt-1"/>Reportes Ventas</NavLink>
+                        </li>
+                        <li className="p-3 hover:bg-slate-500">
+                        <NavLink to="/productos" exact={true} className="flex" activeclassname="active"><FaIcons.FaStopCircle className="mx-3 mt-1"/>Reportes Stock</NavLink>
+                        </li>
+                        <li className="p-3 hover:bg-slate-500">
+                        <NavLink to="/proveedores" exact={true} className="flex" activeclassname="active"><FaIcons.FaStopCircle className="mx-3 mt-1"/>Cuentas Por Cobrar</NavLink>
+                        </li>
+                </ul>
+                )}
                 </li>
                 <li className="p-3"></li>
                 <li className="p-2 hover:bg-slate-600">
---
+++ src/pages/Maintenance/Products.js
@@ -0,0 +1,45 @@
+import React from "react";
+import * as FaIcons from 'react-icons/fa';
+const Products = () =>{
+    return(
+        <div>
+          <section className="w-100 bg-gray-100 p-6">
+            <div className="flex  justify-between mb-5">
+            <h1 className="font-medium text-2xl">Productos</h1>
+            <p className="text-gray-500">Productos/<a className="cursor-pointer text-sky-500 font-medium">Inicio</a></p>
+            </div>
+            <div className="bg-blue-500 text-white p-2 rounded w-48">
+            <a className="flex"><FaIcons.FaPlusCircle className="mx-2 m-1"/>Crear Productos</a>
+            </div>
+          </section>
+            <section className="shadow-lg rounded w-100 h-auto m-3 border border-gray-300">
+                <div className="p-6 flex">
+                    <div>
+                        <p className="flex font-medium mb-2"><FaIcons.FaFilter className="mx-2 m-1"/>Filtro General</p>
+                        <input className="border border-gray-300 m-2 p-1 rounded w-64"></input>
+                    </div>
+                    <div className="bg-gray-300 rounded w-48 h-9 p-1.5 mt-10 border border-gray-400 shadow-sm">
+                        <a className="flex"><FaIcons.FaSearch className="mx-2 m-1"/>Buscar</a>
+                    </div>
+                </div>
+                <div className="border border-gray-300 shadow w-100 h-full m-8 p-6 rounded">
+                    <table className="border w-full">
+                       <thead className="flex p-2 justify-between  bg-slate-400">
+                        <td className="font-medium">Nombre</td>
+                        <td className="font-medium">Codigo</td>
+                        <td className="font-medium">Marca</td>
+                        <td className="font-medium">Linea</td>
+                        <td className="font-medium">Tamaño/Modelo</td>
+                       </thead>
+                       <tbody>
+                        filtro de datos de c#
+                       </tbody>
+                    </table>
+                </div>
+
+            </section>
+            
+        </div>
+    )
+}
+export default Products;
