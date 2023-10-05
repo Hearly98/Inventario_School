dfe601bfa01f9ac2ed61992e6d8527eb0ba4cefe
Sebastian <shuertas981@gmail.com>
2023-10-05 07:21:28 -0500

    Agregando Consideraciones en CreateCotizacion

--- src/pages/Sales/CreateCotizacion.js
+++ src/pages/Sales/CreateCotizacion.js
@@ -4,9 +4,9 @@ import { NavLink } from "react-router-dom";
 
 const CreateCotizacion = () =>{
     const data = [
-        { nombre: 'Producto 1', cantidad: 5, unidadMedida: 'und', precioUnitario: 10, precioTotal: 50 },
-        { nombre: 'Producto 2', cantidad: 3, unidadMedida: 'kl', precioUnitario: 15, precioTotal: 45 },
-        { nombre: 'Producto 3', cantidad: 2, unidadMedida: 'lt', precioUnitario: 8, precioTotal: 16 }
+        { nombre: 'Producto 1', cantidad: 5, unidadMedida: 'und', precioUnitario: 10, dscto:25,precioTotal:60 },
+        { nombre: 'Producto 2', cantidad: 3, unidadMedida: 'kl', precioUnitario: 15,dscto:25, precioTotal:50},
+        { nombre: 'Producto 3', cantidad: 2, unidadMedida: 'lt', precioUnitario: 8, dscto:25,  precioTotal:30 }
      ];
     return(
         //section fondo
@@ -24,50 +24,59 @@ const CreateCotizacion = () =>{
             </div>   
             <div>
             <label className="font-medium">Moneda</label>
-            <select typeof="text">Soles</select>  
+            <select className="border border-gray-400 p-1 flex rounded">
+            <option>SOLES</option>
+            <option>DOLARES</option>
+            </select>  
             </div>  
             </div>
 
             {/*Section Proveedor */} 
 
             <section className=" bg-slate-50 m-4 rounded border h-auto">
-                <h1 className="w-full font-medium text-white text-xl bg-blue-500 p-3">Proveedor</h1>
+                <h1 className="w-full font-medium text-white text-xl bg-blue-500 p-3">Cliente</h1>
 
                 {/* Datos Proveedor */} 
                <div className="grid grid-cols-1 md:grid-cols-2 p-4">
+                   <div className="flex flex-col m-2">
+                    <label className="font-medium flex">Nro. Documento</label>
+                    <div className="flex">
+                    <input className="p-1 border rounded border-gray-400 w-full"/>
+                    <button className="bg-blue-500 rounded p-1"><FaIcons.FaSearch className="text-white mx-2 m-1"/></button>
+                    </div>
+                    </div> 
                     <div className="flex flex-col m-2">
-                    <label className="font-medium flex">Proveedor</label>
+                    <label className="font-medium flex">Cliente</label>
                     <input className="p-1 border rounded border-gray-400"/>
                     </div>
-                    <div className="flex flex-col m-2 w-64">
-                    <label className="font-medium flex">Nro. Documento</label>
-                    <input className="p-1 border rounded border-gray-400"/>
-                    </div> 
+             
                     <div className="flex flex-col m-2 ">
                     <label className="font-medium flex">Dirección</label>
                     <input className="p-1 border rounded border-gray-400"/>
                     </div> 
-                </div> 
-            </section>
-            {/*Section Contacto*/} 
-
-            <section className=" bg-slate-50 m-4 rounded border h-auto">
-
-                <h1 className="w-full font-medium text-white text-xl bg-blue-500 p-3">Contacto</h1>
-
-            {/*Section Datos Contacto */} 
-               <div className="grid grid-cols-1 md:grid-cols-2 p-4">
-                    <div className="flex flex-col m-2">
-                    <label className="font-medium flex">Nombre Contacto</label>
-                    <input className="p-1 border rounded border-gray-400"/>
-                    </div>
-                    <div className="flex flex-col m-2">
-                    <label className="font-medium flex">Area</label>
+                    <div className="flex flex-col m-2 ">
+                    <label className="font-medium flex">Contacto</label>
                     <input className="p-1 border rounded border-gray-400"/>
                     </div> 
                 </div> 
             </section>
-            {/*Section Detalle de OC */} 
+            {/*Section Vendedor*/} 
+
+            <section className=" bg-slate-50 m-4 rounded border h-auto">
+
+                <h1 className="w-full font-medium text-white text-xl bg-blue-500 p-3">Vendedor</h1>
+
+            {/*Section Datos Vendedor */} 
+               <div className="grid grid-cols-1 md:grid-cols-2 p-4">
+                    <div className="flex flex-col m-2">
+                    <label className="font-medium flex">Nombre Vendedor</label>
+                    <select className="border border-gray-400 p-1 rounded w-2/5">
+                        <option>Seleccione vendedor ...</option>
+                    </select>
+                    </div>
+                </div> 
+            </section>
+            {/*Section Detalle de Cotizacion */} 
             <section className=" bg-slate-50 m-4 rounded border">
                 <h1 className="w-full font-medium text-white text-xl bg-blue-500 p-3">Detalle</h1>
                 {/*Section Datos Detalle de OC */} 
@@ -82,15 +91,8 @@ const CreateCotizacion = () =>{
                 {/*Section Condicionales para Detalle OC*/}
                 <div className="grid grid-cols-1 md:grid-cols-4 text-sm ml-3 m-4">
                 <div className="flex flex-col">
-                        <label className="font-medium flex">¿Mostrar UM?</label>
+                        <label className="font-medium flex">¿Incluye I.G.V.?</label>
                         <select className="w-24 border border-gray-400 text-center p-1">
-                            <option>SI</option>
-                            <option>NO</option>
-                        </select>
-                        </div> 
-                        <div className="flex flex-col">
-                        <label className="font-medium flex">¿Mostrar Precios?</label>
-                        <select className="w-24 border border-gray-400 rounded text-center p-1">
                             <option>SI</option>
                             <option>NO</option>
                         </select>
@@ -101,17 +103,10 @@ const CreateCotizacion = () =>{
                             <option>SI</option>
                             <option>NO</option>
                         </select>
-                        </div> 
-                        <div className="flex flex-col">
-                        <label className="font-medium flex">¿Mostrar Firma?</label>
-                        <select className="w-24 border border-gray-400 rounded text-center p-1">
-                            <option>SI</option>
-                            <option>NO</option>
-                        </select>
-                        </div> 
+                        </div>
                     </div> 
                     </div> 
-                    {/*Section Tabla de Productos para OC*/}
+                    {/*Section Tabla de Productos para Cotizacion*/}
                     <div className="border border-gray-300 shadow w-100 h-full m-8 p-6 rounded">
                     <table className="border w-full">
                        <thead>
@@ -120,7 +115,8 @@ const CreateCotizacion = () =>{
                         <th className="font-medium flex-1">Cant.</th>
                         <th className="font-medium flex-1">U.M</th>
                         <th className="font-medium flex-1">P. Unitario</th>
-                        <th className="font-medium flex-1">P. Total</th>
+                        <th className="font-medium flex-1">Dscto</th>
+                        <th className="font-medium flex-1">Precio Total</th>
                         <th className="font-medium flex-1"></th>
                        </tr>
                        </thead>
@@ -132,6 +128,7 @@ const CreateCotizacion = () =>{
                         <td className="flex-1 text-center">{item.cantidad}</td>
                         <td className="flex-1 text-center">{item.unidadMedida}</td>
                         <td className="flex-1 text-center">{item.precioUnitario}</td>
+                        <td className="flex-1 text-center">{item.dscto}</td>
                         <td className="flex-1 text-center">{item.precioTotal}</td>
                         {/* botones */}
                         <td><button className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded flex"><FaIcons.FaTrash className="mx-2 m-1"/>Remover</button></td>
@@ -158,8 +155,43 @@ const CreateCotizacion = () =>{
             {/*Section Consideraciones */} 
              <section className=" bg-slate-50 m-4 rounded border h-auto">
                 <h1 className="w-full font-medium text-white text-xl bg-blue-500 p-3">Consideraciones</h1>
-               <div className="m-4">
-               <textarea className="border border-gray-400 p-3 w-full h-auto rounded"/>
+               <div className="grid grid-cols-1 md:grid-cols-2 p-4">
+                <div className="flex flex-col-2 m-2">
+                    <label className="font-medium flex">Tipo de Pago</label>
+                    <select className="">
+                        <option>CONTADO</option>
+                        <option>CREDITO</option>
+                        <option>CONTRAENTREGA</option>
+                    </select>
+                    <span>¿Mostrar?</span><input type="checkbox"/>
+                </div>
+                <div className="flex flex-col-2 m-2">
+                    <label className="font-medium flex">Plazo de Entrega</label>
+                    <textarea></textarea>
+                    <span>¿Mostrar?</span><input type="checkbox"/>
+                </div>
+                <div className="flex flex-col-2 m-2">
+                    <label className="font-medium flex">Validez de la Oferta</label>
+                    <textarea></textarea>
+                    <span>¿Mostrar?</span><input type="checkbox"/>
+                </div>
+                <div className="flex flex-col-2 m-2">
+                    <label className="font-medium flex">Consideraciones para la entrega del producto</label>
+                    <textarea></textarea>
+                    <span>¿Mostrar?</span><input type="checkbox"/>
+                </div>
+                <div className="flex flex-col-2 m-2">
+                    <label className="font-medium flex">Tipo de Pago</label>
+                    <input />
+                    <span>¿Mostrar?</span><input type="checkbox"/>
+                </div>
+                <div className="flex flex-col-2 m-2">
+                    <label className="font-medium flex">Mostrar Cuentas Bancarias</label>
+                    <select>
+                        <option>SI</option>
+                        <option>NO</option>
+                    </select>
+                </div>
                </div>
             </section>
             {/*Section botones de guardado*/}
